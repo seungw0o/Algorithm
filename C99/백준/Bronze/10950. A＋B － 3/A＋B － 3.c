3ff697d403e704e3d0e303bad247d2d6f009a5a1
정승우 <122603616+seungw0o@users.noreply.github.com>
2024-05-27 08:44:40 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int n,a,b;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+	return 0;
+}
