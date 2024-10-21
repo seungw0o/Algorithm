6beafe97d2b4975211f27e4d6b5eebd31edce500
정승우 <122603616+seungw0o@users.noreply.github.com>
2024-10-21 16:48:54 +0900

    [Bronze I] Title: 0의 개수, Time: 44 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11170. 0의 개수/0의 개수.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int check_0(int n, int m) {
+	int cnt = 0;
+	for (int i = n; i <= m; i++) {
+		if (i == 0) {
+			cnt++;
+		}
+		int i2 = i;
+
+		while (i2 > 0) {
+			if (i2 % 10 == 0){
+				cnt++;
+			}
+			i2 /= 10;
+		}
+
+	}
+	return cnt;
+}
+
+int main() {
+
+	int T, N, M;
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++) {
+		scanf("%d %d", &N, &M);
+		printf("%d\n", check_0(N, M));
+	}
+
+}
