7b2d860ccaa657bf35621d43ace0abe42bf991a8
정승우 <122603616+seungw0o@users.noreply.github.com>
2024-11-25 08:51:05 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,5 +1,4 @@
 function gcd(a, b) {
-    // 최대 공약수(GCD)를 찾는 함수
     while (b) {
         [a, b] = [b, a % b];
     }
@@ -7,15 +6,15 @@ function gcd(a, b) {
 }
 
 function solution(numer1, denom1, numer2, denom2) {
-    // 두 분수를 같은 분모로 변환하여 더하기
-    const commonDenom = denom1 * denom2; // 공통 분모
-    numer1 *= denom2; // 첫 번째 분자의 분모에 두 번째 분모를 곱함
-    numer2 *= denom1; // 두 번째 분자의 분모에 첫 번째 분모를 곱함
+    
+    const commonDenom = denom1 * denom2; 
+    numer1 *= denom2; 
+    numer2 *= denom1; 
 
-    const numeratorSum = numer1 + numer2; // 분자 합산
+    const numeratorSum = numer1 + numer2; 
 
-    // 기약 분수로 만들기 위해 최대 공약수로 나눔
+    
     const commonGCD = gcd(numeratorSum, commonDenom);
 
-    return [numeratorSum / commonGCD, commonDenom / commonGCD]; // 기약 분수 반환
+    return [numeratorSum / commonGCD, commonDenom / commonGCD]; 
 }
