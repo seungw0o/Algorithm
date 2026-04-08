00d37b6ded97d0377926b891ad04951d21b39114
정승우 <122603616+seungw0o@users.noreply.github.com>
2026-04-08 12:04:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,19 @@
+t = int(input())
+nums = [int(input()) for _ in range(t)]
+
+max_n = max(nums)
+
+dp = [0] * (max_n + 1)
+
+if max_n >= 1:
+    dp[1] = 1
+if max_n >= 2:
+    dp[2] = 2
+if max_n >= 3:
+    dp[3] = 4
+
+for i in range(4, max_n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+for n in nums:
+    print(dp[n])
